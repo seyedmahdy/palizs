93d5317f9e7d12591f10e41638d0d3fdc4e63d51
seyed mahdi <seyedmahdy139@gmail.com>
2025-11-12 15:16:41 +0330

    before linq

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,23 +1,78 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BlazorApp1.Models;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Mvc;
 using static BlazorApp1.Components.Pages.Commodity;
+
 
 namespace BlazorApp1.Controllers
 {
     [ApiController]
     [Route("api/[controller]")] 
-    public class HiController : ControllerBase
+    public class ProductController : ControllerBase
     {
+        private static List<Product> products = new List<Product>
+        {
+            new Product { Id = 1, ProductName = "Laptop", Price = 30000, CategoryName = "Electronics" },
+            new Product { Id = 2, ProductName = "Mouse", Price = 700, CategoryName = "Accessories" }
+        };
 
-        
-        [HttpGet("1")]
-        public IActionResult GetAll()
+        [HttpGet("default")]
+        public IActionResult GetProduct()
         {
-            return Ok("hi");
+            
+            return Ok(products);
         }
-        [HttpGet("2")]
-        public IActionResult GetAll2()
+        [HttpPost("add")]
+        public IActionResult AddProduct([FromBody] Product newProduct)
         {
-            return Ok("hi");
+   
+            newProduct.Id = products.Any() ? products.Max(p => p.Id) + 1 : 1;
+
+            products.Add(newProduct);
+            return Ok(newProduct);
         }
+        [HttpDelete("delete/{id}")]
+        public IActionResult DeleteProduct(int id)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+                return NotFound("محصول پیدا نشد.");
+
+            products.Remove(product);
+
+            return Ok("محصول حذف شد.");
+        }
+        [HttpPut("edit/{id}")]
+        public IActionResult EditProduct(int id, [FromBody] Product updatedProduct)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+                return NotFound("محصول پیدا نشد.");
+            product.ProductName = updatedProduct.ProductName;
+            product.SupplierName = updatedProduct.SupplierName;
+            product.BrandName = updatedProduct.BrandName;
+            product.Email = updatedProduct.Email;
+            product.CategoryName = updatedProduct.CategoryName;
+            product.Supply = updatedProduct.Supply;
+            product.Price = updatedProduct.Price;
+            product.StartDate = updatedProduct.StartDate;
+            product.EndDate = updatedProduct.EndDate;
+            product.ImageName = updatedProduct.ImageName;
+            product.CatalogueName = updatedProduct.CatalogueName;
+            product.Color = updatedProduct.Color;
+
+            return Ok(product);
+        }
+        [HttpGet("show/{id}")]
+        public IActionResult ShowProduct(int id)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+                return NotFound($"محصول با آیدی {id} یافت نشد.");
+
+            return Ok(product);
+        }
+
+
     }
 }
---
+++ Models/Class.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace BlazorApp1.Models
+{
+    public class Product
+    {
+        public int? Id { get; set; }
+        public string ProductName { get; set; }
+        public string? SupplierName { get; set; }
+        public string BrandName { get; set; }
+        public string? Email { get; set; }
+        public string CategoryName { get; set; }
+        public int? Supply { get; set; }
+        public int? Price { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public string ImageName { get; set; }
+        public string? CatalogueName { get; set; }
+        //public IBrowserFile? Image { get; set; }
+        //public IBrowserFile? Catalogue { get; set; }
+        public string? Color { get; set; }
+    }
+}
